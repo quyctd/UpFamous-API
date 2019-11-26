fabb9d8615a1aad4759eb198010c4f507d95af9f
quyctd <quy.dc98@gmail.com>
2019-11-26 23:27:55 +0700

    Implemented register API

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,4 +1,18 @@
 module V1
   class UsersController < ApplicationController
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        render :create, status: :created
+      else
+        head(:unprocessable_entity)
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+    end
   end
 end
