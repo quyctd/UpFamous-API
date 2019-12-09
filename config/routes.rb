feee8a4efadafa09541904e99a4d49bfb9a3e86a
Quý Đinh <quy.dc98@gmail.com>
2019-12-10 05:30:13 +0700

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :v1, default: { format: :json } do
-    resources :sessions, only: [:create, :destroy, :show]
+    resources :sessions, only: %i[create destroy show]
     resources :users, only: [:create]
     resources :collections
     resources :uploads, only: [:create]
-    resources :items
+    get 'items/homepage', to: 'items#homepage_thumbnail'
+    get 'items/all', to: 'items#endless_item'
   end
 end
